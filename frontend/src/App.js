4cd52682aa5b9b3473ff0fc32ae71f2426ae5022
jonesaicayahsharal <jonesaicayahsharal@gmail.com>
2026-01-07 21:04:13 -0500

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -22,7 +22,7 @@ import PlumbingServicePage from "./pages/services/PlumbingServicePage";
 import QuotePage from "./pages/QuotePage";
 
 const BACKEND_URL = process.env.REACT_APP_BACKEND_URL;
-export const API = "https://infrastructure-production-cc30.up.railway.app";
+export const API = "https://infrastructure-production-cc30.up.railway.app/api";
 
 function App() {
   const [showModal, setShowModal] = useState(false);
