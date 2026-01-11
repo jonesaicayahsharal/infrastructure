795a006f3771cbbd7f340daf9dc7598e3823d0ea
jonesaicayahsharal <jonesaicayahsharal@gmail.com>
2026-01-11 17:51:26 -0500

    Update email.py

--- backend/utils/email.py
+++ backend/utils/email.py
@@ -1,1 +1,29 @@
+import os
+from fastapi_mail import FastMail, MessageSchema, ConnectionConfig
+from pydantic import EmailStr
 
+conf = ConnectionConfig(
+    MAIL_USERNAME=os.getenv("EMAIL_USER"),
+    MAIL_PASSWORD=os.getenv("EMAIL_PASS"),
+    MAIL_FROM=os.getenv("EMAIL_FROM"),
+    MAIL_PORT=int(os.getenv("EMAIL_PORT", 587)),
+    MAIL_SERVER=os.getenv("EMAIL_HOST"),
+    MAIL_TLS=True,
+    MAIL_SSL=False,
+    USE_CREDENTIALS=True,
+)
+
+fm = FastMail(conf)
+
+async def send_email(
+    subject: str,
+    recipients: list[EmailStr],
+    body: str,
+):
+    message = MessageSchema(
+        subject=subject,
+        recipients=recipients,
+        body=body,
+        subtype="html",
+    )
+    await fm.send_message(message)
